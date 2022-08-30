ea23382e89e6e63ef59e9e840283c376dc3931de
manojpeddidev <manojpeddi.dev@gmail.com>
2022-08-30 15:25:37 +0530

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductlistComponent } from './pages/productlist/productlist.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ {path: '', component: HomeComponent},
+ {path: 'about', component: AboutusComponent},
+ {path: 'products', component: ProductlistComponent},
+ {path: 'contact', component: ContactComponent},
+ {path: 'blog', component: BlogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProductlistComponent } from './pages/productlist/productlist.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent,
+    ContactComponent,
+    ProductlistComponent,
+    BlogComponent,
   ],
   imports: [
     BrowserModule,
+    CarouselModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
